32389673fa2a4d99e207e1941df4a13cd0c55828
kiratara <raibinita217@gmail.com>
2021-01-11 09:43:45 -0600

    add models- subject, topic, and question, and register with admin page

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Subject, Topic, Question
 
-# Register your models here.
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    '''
+    Register Subject model with the admin.
+    Define the view for Subject. 
+    '''
+    list_display = ['name', 'slug']
+    # automatically set using the value of other fields, slug with name
+    prepopulated_fields = {'slug': ('name',)} 
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    '''
+    Register Topic model with the admin.
+    Define the view for Topic. 
+    '''
+    list_display = ['subject', 'title', 'confidence', 'slug']
+    prepopulated_fields = {'slug': ('title',)} 
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    '''
+    Register Question model with the admin.
+    Define the view for Question. 
+    '''
+    list_display = ['title', 'topic']
+    # prepopulated_fields = {'slug': ('title',)} 
--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,75 @@
 from django.db import models
 
-# Create your models here.
+
+class Subject(models.Model):
+    '''Define a Subject model
+    Subject 
+    '''
+
+    name = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated  = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(max_length=200,
+                            unique=True) # creation of index by unique
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return f"Subject: {self.name}"
+
+    def get_absolute_url(self):
+        '''return url for the subject object'''
+        return reverse('subject:topic_list',
+                       args=[self.slug])
+
+
+class Topic(models.Model):
+    '''Define Topic entity
+    Each subject will have many topics that will then contain the notes
+    '''
+
+    CONFIDENCE_CHOICES = [
+        ('0', 'Nada'), 
+        ('1', 'So-so'),
+        ('2', 'I am Good'),
+        ('4', 'Got this!')]
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    # subject-to-topic is one-to-many relationship. One subject will have many topics
+    subject = models.ForeignKey(Subject, 
+                                on_delete=models.CASCADE, 
+                                related_name='topics')
+    slug = models.SlugField(max_length=200, db_index=True)
+    confidence = models.CharField(max_length=10, choices=CONFIDENCE_CHOICES)
+    notes = models.TextField(blank=True)
+
+    def __str__(self):
+        '''String representation of topic'''
+        return self.title
+
+    def get_absolute_url(self):
+        ''' return url of the object'''
+        return reverse('topic:topic_detail',
+                    args=[self.id, self.slug])
+
+
+class Question(models.Model):
+    '''Define questions class 
+    '''
+    title = models.TextField(blank=True)
+    resolved = models.BooleanField(default=False)
+    # slug = models.SlugField(max_length=200, db_index=True)
+    topic = models.ForeignKey(Topic, 
+                            on_delete=models.CASCADE,
+                            related_name='question')
+
+    # def get_absolute_url(self):
+    #     ''' return url of the object'''
+    #     return reverse('question:question_detail',
+    #                 args=[self.id, self.slug])
+
+    def __str__(self):
+        '''String representation of Question'''
+        return self.title
